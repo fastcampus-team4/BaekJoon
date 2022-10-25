845915f53b2dbee95eb1a48690f6aadb488dff46
Hye-rim Han <hnhyerim@gmail.com>
2022-10-25 15:04:16 +0900

    chapter. Bronze5 18 exercise

--- HyeRim/bronze5_11022(A + B -8).js
+++ HyeRim/bronze5_11022(A + B -8).js
@@ -3,5 +3,5 @@ const input = require('fs').readFileSync(filePath).toString().trim().split('\n')
 
 for(let i = 1; i < input.length; i += 1) {
   const num = input[i].split(' ').map(Number);
-  console.log(`Case #${i}: ${num[0] + num[1]}`)
+  console.log(`Case #${i}: ${num[0]} + ${num[1]} = ${num[0] + num[1]}`)
 }
