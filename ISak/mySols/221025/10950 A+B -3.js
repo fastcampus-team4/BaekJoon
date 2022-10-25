190f2e38e1f939fdfdef109b0c6ae40b06aa5ce5
hwisaac <54179672+hwisaac@users.noreply.github.com>
2022-10-25 14:29:48 +0900

    221025 브5

--- ISak/mySols/221025/10950 A+B -3.js
+++ ISak/mySols/221025/10950 A+B -3.js
@@ -1,11 +1,9 @@
-// https://www.acmicpc.net/problem/9086
 const filePath = process.platform === "linux" ? 0 : "./ISak/input.txt";
 let input = require("fs").readFileSync(filePath).toString().split("\n");
 
 const N = Number(input[0]);
 
-for (i = 1; i <= N; i++) {
-  let txt = input[i];
-  let result = txt[0] + txt[txt.length - 1];
-  console.log(result);
+for (let i = 1; i <= N; i++) {
+  const elt = input[i].split(" ").map(Number);
+  console.log(elt[0] + elt[1]);
 }
---
+++ ISak/mySols/221025/10998 AxB.js
@@ -0,0 +1,9 @@
+const filePath = process.platform === "linux" ? 0 : "./ISak/input.txt";
+let input = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+
+console.log(input[0] * input[1]);
