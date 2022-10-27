5c90d09780c49f22bec12d8cd22b6ddecfc89d85
Hyerim Han <hyerim_han@naver.com>
2022-10-27 16:31:17 +0900

    chapter. Bronze5 5 exercise

--- HyeRim/bronze5_14928(큰 수).js
+++ HyeRim/bronze5_14928(큰 수).js
@@ -1,7 +1,7 @@
 const filePath = process.platform === 'linux' ? 0 : './input.txt'
-const input = require('fs').readFileSync(filePath).toString().trim().map(BigInt);
+const input = require('fs').readFileSync(filePath).toString().trim();
 
-const N = input;
-const num = 20000303;
+const N = BigInt(input);
+const num = BigInt(20000303);
 
-console.log(N % num)
+console.log((N % num).toString())
