565c972d5006116bf124a42d29c62ff40409f75c
Wonny-ing <h.wonny11@gmail.com>
2022-10-28 21:28:57 +0900

    221028 complete

---
+++ HyeWon/mySols/Bronze-V/16394.js
@@ -0,0 +1,7 @@
+// https://www.acmicpc.net/problem/16394
+let input = require('fs').readFileSync(filePath).toString().trim();
+
+const N = Number(input);
+const answer = N - 1946;
+
+console.log(answer);
