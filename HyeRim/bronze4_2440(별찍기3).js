12fb3f183bbc42dd7c804c3bc445b1a01cbccaff
Hye-rim Han <hnhyerim@gmail.com>
2022-11-01 11:30:56 +0900

    Edit Code

--- HyeRim/bronze4_2440(별찍기3).js
+++ HyeRim/bronze4_2440(별찍기3).js
@@ -1,6 +1,9 @@
 const filePath = process.platform === 'linux' ? 0 : './input.txt'
 const input = require('fs').readFileSync(filePath).toString();
 
+let answer = '';
 for(let i = input; i >= 1; i -= 1) {
-  console.log('*'.repeat(i))
+  answer += `${'*'.repeat(i)}\n`
 }
+
+console.log(answer)
