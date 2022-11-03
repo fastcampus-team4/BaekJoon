e1b53d7aff7d619ef23e7927e5e1a4b373f45f95
Wonny-ing <h.wonny11@gmail.com>
2022-11-03 20:19:42 +0900

    221103 complete

---
+++ HyeWon/4470.js
@@ -0,0 +1,9 @@
+// https://www.acmicpc.net/problem/4470
+const filePath = process.platform === 'linus' ? 0 : './input.txt';
+let input = require('fs').readFileSync(filePath).toString().trim().split('\n');
+
+const N = Number(input.shift());
+
+for (let i = 0; i < N; i++) {
+  console.log(`${i + 1}. ${input[i]}`);
+}
---
+++ HyeWon/5524.js
@@ -0,0 +1,9 @@
+// https://www.acmicpc.net/problem/5524
+const filePath = process.platform === 'linus' ? 0 : './input.txt';
+let input = require('fs').readFileSync(filePath).toString().trim().split('\n');
+
+const N = Number(input.shift());
+
+for (let i = 0; i < N; i++) {
+  console.log(`${input[i].toLowerCase()}`);
+}
