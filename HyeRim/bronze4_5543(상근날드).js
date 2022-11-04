52c0a651b1ae902b5aaaae4414212af5e3f040fc
Hye-rim Han <hnhyerim@gmail.com>
2022-11-05 02:39:14 +0900

    chapter. bronze4 6 exercise

--- HyeRim/bronze4_5543(상근날드).js
+++ HyeRim/bronze4_5543(상근날드).js
@@ -1,13 +1,11 @@
 const filePath = process.platform === 'linux' ? 0 : './input.txt'
-const input = require('fs').readFileSync(filePath).toString().split('\n').map(Number);
+const [a,b,c,x,y] = require('fs').readFileSync(filePath).toString().trim().split('\n').map(Number);
 
-let answer = 0;
-for(let i = 0; i < input.length - 2; i += 1) {
-  console.log(input[i])
-  for(let j = 3; j < input.length; j += 1) {
-    console.log(input[j])
-    answer = input[i] + input[j] - 50
-    console.log(answer)
-  }
-}
 
+const burger = [a,b,c].sort((a,b)=>b-a).pop()
+console.log(burger)
+
+const drink = [x,y].sort((a,b)=>b-a).pop()
+console.log(drink)
+
+console.log((burger + drink) - 50)
