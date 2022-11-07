1e52857fcc241d8ff743e58b1ad148946539c88d
Wonny-ing <h.wonny11@gmail.com>
2022-11-07 14:24:10 +0900

    fix: 14623|감정이입

--- HyeWon/14623.js
+++ HyeWon/14623.js
@@ -1,13 +1,7 @@
 // https://www.acmicpc.net/problem/14623
 const filePath = process.platform === 'linux' ? 0 : './input.txt';
-const [b1, b2] = require('fs').readFileSync(filePath).toString().trim().split('\n').map(BigInt);
-b1;
+const [b1, b2] = require('fs').readFileSync(filePath).toString().trim().split('\n');
+
 const mul = BigInt('0b' + b1) * BigInt('0b' + b2);
-
-const answer = Number(mul.toString(2));
+const answer = mul.toString(2);
 console.log(answer);
-
-// const mul = (BigInt("0b" + b1.toString()) * BigInt("0b" + b2.toString()));
-
-// const answer = Number(mul.toString(2));
-// console.log(answer);
