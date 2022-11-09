9c99888a325b29c05409c07ced26a7839435f54a
zwonkim <k980912@naver.com>
2022-11-09 17:30:35 +0900

    Day12 과제 제출

---
+++ Jiwon/Day12/4470번.js
@@ -0,0 +1,13 @@
+const filePath =
+  process.platform === "linux"
+    ? 0
+    : "/Users/gimjiwon/Desktop/fastcampus-study/baekjoon/BaekJoon/Jiwon/Day12/input.txt";
+const inputData = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n");
+
+for (let i = 1; i <= inputData[0]; i++) {
+  console.log(`${i}. ${inputData[i]}`);
+}
