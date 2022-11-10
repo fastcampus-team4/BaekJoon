566cf451cea655604538e845fc53ea88db9e5f68
hae9 <gorud0929@naver.com>
2022-11-10 12:01:35 +0900

    221110 - bronze4, Q5 (1)

--- Haekyung/221110/24263.js
+++ Haekyung/221110/24263.js
@@ -2,3 +2,6 @@ const { format } = require('path');
 
 const filePath = process.platform === 'linux' ? 0 : 'input.txt';
 const n = require('fs').readFileSync(filePath).toString().trim();
+
+console.log(n);
+console.log(1);
