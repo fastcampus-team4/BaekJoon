6def632e7bfda30ca0cd49bf391981a7368de89f
Hye-rim Han <hnhyerim@gmail.com>
2022-11-13 02:38:13 +0900

    style

--- HyeRim/bronze4_23825(SASA 모형을 만들어보자).js
+++ HyeRim/bronze4_23825(SASA 모형을 만들어보자).js
@@ -2,4 +2,4 @@ const filePath = process.platform === 'linux' ? 0 : './input.txt'
 let [N, M] = require('fs').readFileSync(filePath).toString().trim().split(' ').map(Number);
 
 
-console.log(Math.floor(Math.min((N, M)) / 2));
+console.log(Math.floor(Math.min(N, M) / 2));
